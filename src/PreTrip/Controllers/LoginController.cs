5f70adf16e125545bcb39c04eeac21f28ec011c6
Higor Senna <higorr_jf@hotmail.com>
2016-08-27 20:35:35 -0300

    Iniciado layout administrativo

---
+++ src/PreTrip/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using PreTrip.Model.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PreTrip.Controllers
+{
+    public class LoginController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View("Administrativo", new Usuario());
+        }
+
+        [HttpPost]
+        public ActionResult Logar(Usuario usuario)
+        {
+            return View("Administrativo",new Usuario());
+        }
+
+        private ActionResult ValidarUsuario(Usuario usuario)
+        {
+            if (usuario.IsAdmin)
+            {
+                return View("Login/Administrativo", usuario);
+            }
+
+            return RedirectToAction("Index","Home", usuario);
+        }
+    }
+}

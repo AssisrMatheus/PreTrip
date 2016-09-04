19d31f1b05c534bd252c3da6d668d8683e877129
Higor Senna <higorr_jf@hotmail.com>
2016-09-04 13:08:20 -0300

    Adicionado usuario na tabela busca

--- src/PreTrip.Model/Classes/Busca.cs
+++ src/PreTrip.Model/Classes/Busca.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -9,16 +10,19 @@ namespace PreTrip.Model.Classes
 {
     [Table("Busca")]
     public class Busca
-    {
+    {             
         public Busca()
         {
             DtHrBusca = DateTime.Now;
         }
 
+        [Key]
         public int Id { get; set; }
 
         public string Termo { get; set; }
 
         public DateTime DtHrBusca { get; set; }
+
+        public Usuario Usuario;
     }
 }

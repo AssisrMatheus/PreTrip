43e6f9eec02537c278427816ca468fba96d99ed3
Matheus Assis <adowtatep@hotmail.com.br>
2016-09-04 21:14:33 -0300

    Pequenos ajustes

--- src/PreTrip/Services/Viagens/ViagensService.cs
+++ src/PreTrip/Services/Viagens/ViagensService.cs
@@ -43,6 +43,7 @@ namespace PreTrip.Services.Viagens
                                join destino in db.Enderecos on viag.Destino.Id equals destino.Id
                                join origem in db.Enderecos on viag.Origem.Id equals origem.Id
                                join emp in db.Empresas on viag.Empresa.Id equals emp.Id
+                               join veic in db.Veiculos on viag.Veiculo.Id equals veic.Id
 
                                where
                                //se o filtro estiver nulo, ele busca o que esta no banco,exemplo, se filtro origem estiver nulo, para cada linha ira buscar origem = origem do banco
@@ -54,11 +55,18 @@ namespace PreTrip.Services.Viagens
 
                                select new Viagem()
                                {
-                                   Origem = origem,
                                    Id = viag.Id,
-                                   PrecoPassagem = viag.PrecoPassagem,
+                                   Titulo = viag.Titulo,
                                    Descricao = viag.Descricao,
-                                   Destino = destino
+                                   Destino = destino,
+                                   Origem = origem,
+                                   Empresa = emp,
+                                   Veiculo = veic,
+                                   DtHrChegadaEstimada = viag.DtHrChegadaEstimada,
+                                   DtHrSaida = viag.DtHrSaida,
+                                   PrecoPassagem = viag.PrecoPassagem,
+                                   QuantidadeLugaresDisponiveis = viag.QuantidadeLugaresDisponiveis,
+                                   UrlImagem = viag.UrlImagem
                                }).ToList();
 
                  return viagens;
@@ -78,16 +86,26 @@ namespace PreTrip.Services.Viagens
         {
             using (var db = new PreTripDB())
             {
-                return from viag in db.Viagens.ToList()
-                       join dest in db.Enderecos.ToList()
-                       on viag.Destino equals dest
-                       select new Viagem()
-                       {
-                           Id = viag.Id,
-                           PrecoPassagem = viag.PrecoPassagem,
-                           Descricao = viag.Descricao,
-                           Destino = dest
-                       };
+                return (from viag in db.Viagens.ToList()
+                        join dest in db.Enderecos on viag.Destino.Id equals dest.Id
+                        join orig in db.Enderecos on viag.Origem.Id equals orig.Id
+                        join emp in db.Empresas on viag.Empresa.Id equals emp.Id
+                        join veic in db.Veiculos on viag.Veiculo.Id equals veic.Id
+                        select new Viagem()
+                        {
+                            Id = viag.Id,
+                            Titulo = viag.Titulo,
+                            Descricao = viag.Descricao,
+                            Destino = dest,
+                            Origem = orig,
+                            Empresa = emp,
+                            Veiculo = veic,
+                            DtHrChegadaEstimada = viag.DtHrChegadaEstimada,
+                            DtHrSaida = viag.DtHrSaida,
+                            PrecoPassagem = viag.PrecoPassagem,
+                            QuantidadeLugaresDisponiveis = viag.QuantidadeLugaresDisponiveis,
+                            UrlImagem = viag.UrlImagem
+                        }).ToList();
             }
         }
 
